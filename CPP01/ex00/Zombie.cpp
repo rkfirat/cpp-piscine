cd4584625caa9713de3ee893885a733854c7aa68
Recep Kamil Fırat <rfirat@k1m43s01.42istanbul.com.tr>
2025-06-29 21:52:33 +0300

    CPP01-Ex00

---
+++ CPP01/ex00/Zombie.cpp
@@ -0,0 +1,17 @@
+#include "Zombie.hpp"
+
+
+Zombie::Zombie(std::string name) : name(name)
+{
+
+}
+
+Zombie::~Zombie(void)
+{
+    std::cout << this->name << " is destroyed!" << std::endl;
+}
+
+void Zombie::announce(void)
+{
+    std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
