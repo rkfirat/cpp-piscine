add096ba14c724438120bbdf8a87888c3ce18ef8
Coni Bravo <coni@Conis-MacBook-Air.local>
2025-09-21 00:27:35 +0300

    Corrections

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -5,49 +5,22 @@
 template <typename T>
 class MutantStack : public std::stack<T>
 {
-private:
-public:
-    MutantStack() : std::stack<T>()
-    {
-        
-    }
-    MutantStack(const MutantStack& other) : std::stack<T>(other)
-    {
+    private:
 
-    }
-    MutantStack& operator=(const MutantStack& other)
-    {
-        if (this != &other)
-        {
-            std::stack<T>::operator=(other);
-        }
-        return *this;
-    }
-    ~MutantStack()
-    {
+    public:
 
-    }
+        typedef typename std::stack<T>::container_type container_type;
+        typedef typename container_type::iterator iterator;
+        typedef typename container_type::const_iterator const_iterator;
+        MutantStack();
+        MutantStack(const MutantStack& other);
+        MutantStack& operator=(const MutantStack& other);
+        ~MutantStack();
 
-    typedef typename std::stack<T>::container_type::iterator iterator;
-    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-
-    iterator begin()
-    {
-        return this->c.begin();
-    }
-
-    iterator end()
-    {
-        return this->c.end();
-    }
-
-    const_iterator begin() const
-    {
-        return this->c.begin();
-    }
-
-    const_iterator end() const
-    {
-        return this->c.end();
-    }
+        iterator begin();
+        iterator end();
+        const_iterator begin() const;
+        const_iterator end() const;
 };
+
+#include "MutantStackt.tpp"
